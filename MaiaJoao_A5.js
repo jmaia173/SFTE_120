57955bc660221cb7fb38a2eba2489bfead76b6f1
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-03-19 09:10:49 -0700

    a

--- MaiaJoao_A5.js
+++
@@ -1,9 +0,0 @@
-function isPalindrome(str) {
-    const cleanedStr = str.replace(/\s+/g, '').toLowerCase();
-    const reversedStr = cleanedStr.split('').reverse().join('');
-    
-    return cleanedStr === reversedStr;
-}
-
-console.log(isPalindrome("racecar")); 
-console.log(isPalindrome("hello")); 
